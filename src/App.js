7dfb858cf579d8a3c6b2b945679981374aa2ab85
Mohamed Elshety <mohamedkingman99@gmail.com>
2023-03-19 16:29:27 +0200

    Edit Design

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
-import React from 'react';
-import { createHashRouter, RouterProvider, useLocation, useNavigate } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home/Home';
-import Card from './components/Cards/Card';
+import React from "react";
+import {
+  createHashRouter,
+  RouterProvider,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Card from "./components/Cards/Card";
 function App() {
- let routers =  createHashRouter([
-  {index : true ,element :<Home/>},
-  {path : '/home', element : <Card/>},
-  {path : '/carditem/:id', element : <FullPageElement />},
-  ])
-  return (
-    <RouterProvider router={ routers}>
-
-    </RouterProvider>
-  );
+  let routers = createHashRouter([
+    { index: true, element: <Home /> },
+    { path: "/home", element: <Card /> },
+    { path: "/carditem/:id", element: <FullPageElement /> },
+  ]);
+  return <RouterProvider router={routers}></RouterProvider>;
 }
 
+const FullPageElement = (props) => {
+  const location = useLocation();
+  const navigate = useNavigate();
 
-const FullPageElement = (props)=>{
- 
-  const location = useLocation()
-  const navigate = useNavigate()
- 
   return (
-    <div style={{width: '100%', height: '50%',display:'flex',cursor:'pointer',margin:'auto', marginTop:'20px'}} onClick={()=>navigate('/')}>
-    <img  alt={location.state.id} src={location.state.image} style={{width: '50%', height: '50%' ,margin:'auto'}} />
+    <div
+      style={{
+        width: "90%",
+        height: "50%",
+        display: "flex",
+        padding: "20px",
+        margin: "auto",
+        cursor: "pointer",
+        marginTop: "15rem",
+        position: "relative",
+        border: "1px solid",
+        borderRadius: "10%",
+      }}
+      onClick={() => navigate("/")}
+    >
+      <span
+        style={{
+          position: "absolute",
+          top: "1%",
+          right: "10%",
+          fontSize: "25px",
+        }}
+      >
+        {location.state.id}{" "}
+      </span>
+      <img
+        alt={location.state.id}
+        src={location.state.image}
+        style={{ width: "100%", height: "50%", margin: "auto" }}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
