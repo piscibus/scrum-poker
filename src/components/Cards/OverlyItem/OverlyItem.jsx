8c00a32e451505310e8445adb32c30a9d156cf80
Mohamed Elshety <mohamedkingman99@gmail.com>
2023-03-28 12:00:02 +0200

    Remove the console.log statement in OvelyItem.jsx

--- src/components/Cards/OverlyItem/OverlyItem.jsx
+++ src/components/Cards/OverlyItem/OverlyItem.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import classes from "./overly-item.module.css";
 const OverlyItem = (props) => {
-  console.log(props.image);
   return (
     <div className={classes.overly}>
       <div className={classes.cover}></div>
