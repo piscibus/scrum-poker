10268288498d5c2d98909c33e0ff88e49590b792
Mohamed Elshety <mohamedkingman99@gmail.com>
2023-04-09 00:07:36 +0200

    fix error path directory

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../Cards/Card";
-import classes from "./Home.module.css";
+import classes from './Home.module.css';
 const Home = () => {
   return (
     <div>
