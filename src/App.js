300faff3ba0b4ba3ff8857b0b6cb55ce9b18d4a6
Mohamed Elshety <49659759+imelshety@users.noreply.github.com>
2023-04-10 12:42:16 +0200

    remove bootstrap (#7)
    * Update Home.jsx
    
    * remove bootstrap classes
    
    remove bootstrap classes  and add custom style
    
    * Remove bootstrap file
    
    * fix error path directory
    
    * update styling in components
    
    * change inline style to external style in App.js
    
    * chore: ignore IDE files
    
    * chore: fix styling
    
    ---------
    
    Co-authored-by: Dhemy <imdhemy@gmail.com>

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import Card from "./components/Cards/Card";
 function App() {
-  const  router = createHashRouter([
+  const router = createHashRouter([
     { index: true, element: <Home /> },
     { path: "/home", element: <Card /> },
     { path: "/carditem/:id", element: <FullPageElement /> },
@@ -22,35 +22,12 @@ const FullPageElement = (props) => {
   const navigate = useNavigate();
 
   return (
-    <div
-      style={{
-        width: "90%",
-        height: "50%",
-        display: "flex",
-        padding: "20px",
-        margin: "auto",
-        cursor: "pointer",
-        marginTop: "15rem",
-        position: "relative",
-        border: "1px solid",
-        borderRadius: "10%",
-      }}
-      onClick={() => navigate("/")}
-    >
-      <span
-        style={{
-          position: "absolute",
-          top: "1%",
-          right: "10%",
-          fontSize: "25px",
-        }}
-      >
-        {location.state.id}{" "}
-      </span>
+    <div className="full-page-element" onClick={() => navigate("/")}>
+      <span className="full-page-sapn">{location.state.id} </span>
       <img
         alt={location.state.id}
         src={location.state.image}
-        style={{ width: "100%", height: "50%", margin: "auto" }}
+        className="full-page-img"
       />
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Card from "../Cards/Card";
+import classes from './Home.module.css';
 const Home = () => {
   return (
     <div>
-      <h2
-        style={{ textShadow: "3px 3px 3px #000000" }}
-        className="text-center  mb-50 bg-dark text-white my-2 p-2"
-      >
-        Choose a Card
-      </h2>
-      <div className="container ">
+      <h2 className={classes["title-primary"]}>Choose a Card</h2>
+      <div className={classes["container"]}>
         <Card />
       </div>
     </div>
