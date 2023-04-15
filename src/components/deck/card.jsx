1e087cf1050f535a256d777adf47932121a3d1d4
Dhemy <imdhemy@gmail.com>
2023-04-15 02:37:50 +0200

    feat: display card number and suit

--- src/components/deck/card.jsx
+++ src/components/deck/card.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
 function Card({card}) {
+  const number = card.number === Infinity ? '∞' : card.number;
+  const suitImg = require(`../../../assets/images/${card.suit}.svg`);
   return (
-      <div>{card}</div>
+      <div className="grid-item play-card">
+        <div className="play-card__body">
+          <div className="play-card__number">
+            <span>{number}</span>
+          </div>
+          <div className="play-card__suit">
+            <img src={suitImg} alt={card.suit}/>
+          </div>
+        </div>
+      </div>
   );
 }
 
--- src/components/deck/deck.jsx
+++ src/components/deck/deck.jsx
@@ -2,18 +2,38 @@ import React from 'react';
 import Card from './card';
 
 const cards = [
-  'pawn',
-  'bishop',
-  'knight',
-  'rook',
-  'queen',
-  'thinker',
+  {
+    number: 1,
+    suit: 'pawn',
+  },
+  {
+    number: 2,
+    suit: 'bishop',
+  },
+  {
+    number: 3,
+    suit: 'knight',
+  },
+  {
+    number: 5,
+    suit: 'rook',
+  },
+  {
+    number: 8,
+    suit: 'queen',
+  },
+  {
+    number: Infinity,
+    suit: 'thinker',
+  },
 ];
 
 function Deck() {
   return (
       <main className="deck">
-        {cards.map((card, index) => (<Card key={index} card={card}/>))}
+        <div className="grid">
+          {cards.map((card, index) => (<Card key={index} card={card}/>))}
+        </div>
       </main>
   );
 }
